44d7209a3cc06b21d569c423089b834235eef91d
Anibalvelozo <82519110+Anibalvelozo@users.noreply.github.com>
2024-02-21 02:39:46 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,62 +10,62 @@ $(window).scroll(function () {
         $('#menu').addClass("navbar");
     }
 
-
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-
-    $(function () {
-        $('[data-toggle="popover"]').popover();
-    });
-
-    $(".card").hover(
-        function () {
-            var cardChange = $(this);
-            cardChange.css("color", "black");
-        },
-        function () {
-            var cardChange = $(this);
-            cardChange.css("color", "black");
-        }
-    );
-
-    $(".card1")
-        .mouseover(function () {
-            $(".card1txt").css("color", "#000000");
-        })
-        .mouseleave(function () {
-            $(".card1txt").css("color", "#ffffff");
-        });
-
-    $(".card2")
-        .mouseover(function () {
-            $(".card2txt").css("color", "#000000");
-        })
-        .mouseleave(function () {
-            $(".card2txt").css("color", "#ffffff");
-        });
-
-    $(".card3")
-        .mouseover(function () {
-            $(".card3txt").css("color", "#000000");
-        })
-        .mouseleave(function () {
-            $(".card3txt").css("color", "#ffffff");
-        });
-
-    $(".card4")
-        .mouseover(function () {
-            $(".card4txt").css("color", "#000000");
-        })
-        .mouseleave(function () {
-            $(".card4txt").css("color", "#ffffff");
-        });
-
-    $('#send_email').click(function () {
-
-        alert("Correo enviado correctamente")
-    })
-
 });
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
+$(".card").hover(
+    function () {
+        var cardChange = $(this);
+        cardChange.css("color", "black");
+    },
+    function () {
+        var cardChange = $(this);
+        cardChange.css("color", "black");
+    }
+);
+
+$(".card1")
+    .mouseover(function () {
+        $(".card1txt").css("color", "#000000");
+    })
+    .mouseleave(function () {
+        $(".card1txt").css("color", "#ffffff");
+    });
+
+$(".card2")
+    .mouseover(function () {
+        $(".card2txt").css("color", "#000000");
+    })
+    .mouseleave(function () {
+        $(".card2txt").css("color", "#ffffff");
+    });
+
+$(".card3")
+    .mouseover(function () {
+        $(".card3txt").css("color", "#000000");
+    })
+    .mouseleave(function () {
+        $(".card3txt").css("color", "#ffffff");
+    });
+
+$(".card4")
+    .mouseover(function () {
+        $(".card4txt").css("color", "#000000");
+    })
+    .mouseleave(function () {
+        $(".card4txt").css("color", "#ffffff");
+    });
+
+$('#send_email').click(function () {
+
+    alert("Correo enviado correctamente")
+})
+
+
 
